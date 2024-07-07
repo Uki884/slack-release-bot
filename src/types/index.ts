418238690d595a91bd5202c4c465ef8bc34d7516
Yuki Hayashi <mrchildreny@gmail.com>
2024-07-07 10:39:13 +0900

    chore: Update Slack bot scopes in .dev.vars.example and index.ts
    This commit updates the Slack bot scopes in the .dev.vars.example file and the index.ts file. The new scopes added are chat:write and app_mentions:read. This change is necessary to enable the bot to perform the required actions and access the necessary information in the Slack workspace.

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,4 +1,4 @@
-import { SlackEdgeAppEnv } from "slack-cloudflare-workers";
+import { KV, SlackOAuthAndOIDCEnv } from "slack-cloudflare-workers";
 
 export type ENV = {
   GITHUB_PRIVATE_KEY: string;
@@ -8,4 +8,7 @@ export type ENV = {
   GITHUB_REPO: string;
   STG_RELEASE_EVENT_NAME: string;
   PROD_RELEASE_EVENT_NAME: string;
-} & SlackEdgeAppEnv;
+} & SlackOAuthAndOIDCEnv & {
+  SLACK_INSTALLATIONS: KV;
+  SLACK_OAUTH_STATES: KV;
+}
