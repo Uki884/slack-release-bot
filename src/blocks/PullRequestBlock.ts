12bb878aec7eb4e7be73d56fc60b95d09d7daf92
Yuki Hayashi <mrchildreny@gmail.com>
2024-07-07 09:23:22 +0900

    feat: Add PullRequestBlock and ReleaseListBlock components
    This commit adds two new components, PullRequestBlock and ReleaseListBlock, to the codebase. PullRequestBlock is responsible for generating a context block for each pull request, including information such as the pull request number, title, mergeability, author, and base/head refs. ReleaseListBlock creates a header section and staging release buttons for the release list command. These components enhance the functionality and appearance of the application.

---
+++ src/blocks/PullRequestBlock.ts
@@ -0,0 +1,31 @@
+import { ContextBlock } from "slack-cloudflare-workers";
+import { PullRequest } from "../api/types/graphqlApiTypes";
+
+export const PullRequestBlock =  (pullRequests: PullRequest[]) => {
+  return pullRequests.map((pullRequest, index) => {
+    const context: ContextBlock = {
+      type: "context",
+      block_id: `${pullRequest.number}`,
+      elements: [
+        {
+          type: "mrkdwn",
+          text: `${index + 1}. <${pullRequest.url}|#${pullRequest.number} *${pullRequest.title}*> (${pullRequest.mergeable})`,
+        },
+        {
+          type: "image",
+          image_url: pullRequest.author.avatarUrl,
+          alt_text: pullRequest.author.login,
+        },
+        {
+          type: "mrkdwn",
+          text: `<${pullRequest.author.url}|*${pullRequest.author.login}*>`,
+        },
+        {
+          type: "mrkdwn",
+          text: `(${pullRequest.baseRef.name} <- ${pullRequest.headRef.name})`,
+        },
+      ],
+    };
+    return context;
+  });
+};
--- src/commands/update-release-list.ts
+++ src/commands/update-release-list.ts
@@ -1,10 +1,10 @@
-import { ActionsBlock, ContextBlock, SectionBlock, SlackApp } from "slack-cloudflare-workers";
+import { SlackApp } from "slack-cloudflare-workers";
 import { ENV } from "../types";
 import { GithubApi } from "../api/githubApi";
 import { ACTION_ID_LIST } from "../constants/ACTION_ID_LIST";
-import { BLOCK_ID_LIST } from "../constants/BLOCK_ID_LIST";
 import { DividerBlock } from "../blocks/DividerBlock";
-import { COMMAND_LIST } from "../constants/COMMAND_LIST";
+import { PullRequestBlock } from "../blocks/PullRequestBlock";
+import { ReleaseListBlock } from "../blocks/ReleaseListBlock";
 
 export const updateReleaseList = (app: SlackApp<ENV>) => {
   return app.action(
@@ -16,64 +16,8 @@ export const updateReleaseList = (app: SlackApp<ENV>) => {
       const api = GithubApi.new(app.env);
       const approvedPrList = await api.getApprovedPrList();
 
-      const pullRequests = approvedPrList.map((pullRequest, index) => {
-        const context: ContextBlock = {
-          type: "context",
-          block_id: `${pullRequest.number}`,
-          elements: [
-            {
-              type: "mrkdwn",
-              text: `${index + 1}. <${pullRequest.url}|#${pullRequest.number} *${pullRequest.title}*> (${pullRequest.mergeable})`,
-            },
-            {
-              type: "image",
-              image_url: pullRequest.author.avatarUrl,
-              alt_text: pullRequest.author.login,
-            },
-            {
-              type: "mrkdwn",
-              text: `<${pullRequest.author.url}|*${pullRequest.author.login}*>`,
-            },
-            {
-              type: "mrkdwn",
-              text: `(${pullRequest.baseRef.name} <- ${pullRequest.headRef.name})`,
-            },
-          ],
-        };
-        return context;
-      });
-
-      const header: SectionBlock = {
-        type: "section",
-        text: {
-          type: "mrkdwn",
-          text: `<!subteam^S01EKMNQVS9>\nリリースできそうなPRはこちらです！😃\n実行者: <@${req.context.userId}>`,
-        },
-        accessory: {
-          type: "button",
-          text: {
-            type: "plain_text",
-            text: "リスト更新",
-          },
-          action_id: ACTION_ID_LIST.UPDATE_PR_LIST_ACTION,
-        },
-      };
-
-      const stagingReleaseButtons: ActionsBlock = {
-        type: "actions",
-        block_id: BLOCK_ID_LIST.DEPLOY_BUTTON_FOR_STAGING_BLOCK,
-        elements: [
-          {
-            type: "button",
-            text: {
-              type: "plain_text",
-              text: "Deploy Staging",
-            },
-            value: "ok",
-            action_id: COMMAND_LIST.SHOW_STAGING_MODAL_ACTION,
-          },
-        ],
-      };
+      const pullRequests = PullRequestBlock(approvedPrList);
+      const { header, stagingReleaseButtons } = ReleaseListBlock({ userId: req.context.userId });
 
       if (req.context.respond) {
         await req.context.respond({
